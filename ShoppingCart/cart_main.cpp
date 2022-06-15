c41faca102e55fee0934fd994490908a9f1a240e
rksharma333 <102408111+rksharma333@users.noreply.github.com>
2022-06-15 10:39:53 +0530

    Add files via upload

---
+++ ShoppingCart/cart_main.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<vector>
+#include "datamodel.h"
+using namespace std;
+
+vector<Product> allProducts ={
+            Product(1,"apple",26),
+            Product(3,"mango",16),
+            Product(2,"guava",36),
+            Product(5,"banana",56),
+            Product(4,"strawberry",29),
+            Product(6,"pineapple",20),      
+      };
+      Product* chooseProduct(){
+            string productList;
+            cout<<"Available Products "<<endl;
+            for(auto product : allProducts){
+                  productList.append(product.getDisplayedName());
+            }
+            cout << productList <<endl;
+            
+            cout<<"----------------------------"<<endl;
+            string choice;
+            cin >> choice;
+            
+            for(int i=0; i<allProducts.size(); i++){
+                  if(allProducts[i].getShortName() == choice){
+                        return &allProducts[i];
+                  }
+            }
+            cout<<"product not found!" <<endl;
+            return NULL;
+            
+      }
+
+bool checkout(Cart &cart){
+      if(cart.isEmtpy()){
+         return false;   
+      }
+      int total = cart.getTotal();
+      cout<<"Pay in cash ";
+      
+      int paid;
+      cin>>paid;
+      if(paid >= total){
+            cout<<"Change "<<paid-total <<endl;
+            cout<<"Thank you for shopping! ";
+            return true;
+      }
+      else{
+            cout<<"Not enough cash!";
+            return false;
+      }
+}
+
+int main(){
+      
+      char action;
+      Cart cart;
+      while(true){
+            cout<<"Select an action - (a)dd item, (v)iew cart, (c)heckout" <<endl;
+            cin>>action;
+            
+            if(action == 'a'){
+                  //todo: Add to cart
+                  // view all product
+                  
+                  Product * product = chooseProduct();
+                  if(product != NULL){
+                        cout <<"Added to the Cart "<<product->getDisplayedName() <<endl;
+                        cart.addProduct(*product);
+                  }
+                  
+            }
+            else if(action == 'v'){
+                  // View the cart
+                  
+                  cout<<"-----------------------------"<<endl;
+                  cout<< cart.viewCart();
+                  cout<<"-----------------------------"<<endl;
+            }
+            else{
+                 cart.viewCart();
+                 if(checkout(cart)){
+                  break;
+                 } 
+            }
+      }
+      
+      
+    return 0;
+}
---
+++ ShoppingCart/datamodel.h
@@ -0,0 +1,106 @@
+// Product, Item, Cart
+#include <string>
+#include<unordered_map>
+using namespace std;
+
+// Forward Declaration
+class Item;
+class Cart;
+
+class Product
+{
+      int id;
+      string name;
+      int price;
+
+public:
+      Product(){
+            
+      }
+      Product(int u_id, string name, int price)
+      {
+            id = u_id;
+            this->name = name; // this is use to tell compiler to this belong to object
+            this->price = price;
+      }
+      string getDisplayedName()
+      {
+
+            return name + " : Rs " + to_string(price) + "\n"; // error
+      }
+
+      string getShortName()
+      {
+            return name.substr(0, 1);
+      }
+
+      friend class Item; // Item can acces private element of Product
+      friend class Cart;
+};
+
+class Item
+{
+
+      Product product;
+      int quantity;
+
+public:
+      Item(){}
+      // constructor using a Init List
+      Item(Product p, int q) : product(p), quantity(q) {}
+
+      int getItemPrice()
+      {
+            return quantity * product.price;
+      }
+      string getItemInfo()
+      {
+            return to_string(quantity) + " x " + product.name + " Rs. " + to_string(quantity * product.price) + "\n";
+      }
+      friend class Cart;
+};
+
+class Cart
+{
+
+      unordered_map<int, Item> items;
+
+public:
+      void addProduct(Product product)
+      {
+            if (items.count(product.id) == 0)
+            {
+                  Item newItem(product, 1);
+                  items[product.id] = newItem;
+            }
+            else
+            {
+                  items[product.id].quantity += 1;
+            }
+      }
+      int getTotal(){
+           int total = 0; 
+           for(auto itemPair : items){
+                  auto item = itemPair.second;
+                 total += item.getItemPrice();
+            } 
+            return total;
+      }
+      
+      string viewCart(){
+            if(items.empty()){
+                  return "Cart is empty";
+            }
+            string itemizedList;
+            int cart_total = getTotal(); 
+            
+            for(auto itemPair : items){
+                  auto item = itemPair.second;
+                  itemizedList.append(item.getItemInfo());
+            }
+            return itemizedList + "\n Total Amount : Rs. " + to_string(cart_total) + "\n";
+      }
+      bool isEmtpy(){
+           return items.empty();
+      }
+};
